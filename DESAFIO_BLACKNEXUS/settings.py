f479be0435de42471042a74f72a99cc2f074796c
Your Name <you@example.com>
2023-12-19 02:00:08 -0300

    cambios3

--- DESAFIO_BLACKNEXUS/settings.py
+++ DESAFIO_BLACKNEXUS/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     #'eventApp.apps.EventAppConfig',
     'eventApp',
     'public.apps.PublicConfig',
